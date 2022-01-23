ca962dae76d0b674342795cbfcdc0cf1759346f8
Nicolas Allera <nicoallera@hotmail.com>
2022-01-23 17:35:14 -0300

    Added support for rectangles

--- figuregenerator.py
+++ figuregenerator.py
@@ -8,19 +8,23 @@ def define_nails(figure, figureparams, number_of_nails):
         x, y = generate_ellipse_points(figureparams[0], figureparams[1], number_of_nails)
         nails = [(i, j) for i, j in zip(x, y)]
     if figure == "r":
-        lengths = linspace(0, 4, number_of_nails)
+        total_length = 2 * figureparams[0] + 2 * figureparams[1]
+        first_turn = figureparams[0]
+        second_turn = figureparams[0] + figureparams[1]
+        third_turn = 2 * figureparams[0] + figureparams[1]
+        lengths = linspace(0, total_length, number_of_nails)
         for length in lengths:
-            if length < 1:
-                nails.append((-1 + length * 2, 1))
+            if length < first_turn:
+                nails.append((-figureparams[0] + length * 2, figureparams[1]))
                 continue
-            if length < 2:
-                nails.append((1, 1 - (length - 1) * 2))
+            if length < second_turn:
+                nails.append((figureparams[0], figureparams[1] - (length - first_turn) * 2))
                 continue
-            if length < 3:
-                nails.append((1 - (length - 2) * 2, -1))
+            if length < third_turn:
+                nails.append((figureparams[0] - (length - second_turn) * 2, -figureparams[1]))
                 continue
             else:
-                nails.append((-1, -1 + (length - 3) * 2))
+                nails.append((-figureparams[0], -figureparams[1] + (length - third_turn) * 2))
                 continue
 
     return nails
